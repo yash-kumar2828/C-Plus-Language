bc8936cec4e203112cde130c4c171602ff370a69
yash kumar <you@example.com>
2025-07-08 15:47:36 +0530

    area of circle

---
+++ 04_area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float PI=3.14,r,area;
+    cout<<"enter a radius=";
+    cin>>r;
+    area=PI*r*r;
+    cout<<"area of circle is "<<area;
+    return 0;
+}
