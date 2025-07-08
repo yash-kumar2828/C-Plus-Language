a1d0cbdb2f7bd7c4f7c444ac62ccd90fce1a8567
yash kumar <you@example.com>
2025-07-08 15:47:11 +0530

    arithmetic operator

---
+++ 03_arithmeticOperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter first number=";
+    cin>>x;
+    cout<<"enter second number=";
+    cin>>y;
+    cout<<"addition of "<<x<<" and "<<y<<" is "<<x+y<<endl;
+    cout<<"subtraction of "<<x<<" and "<<y<<" is "<<x-y<<endl;
+    cout<<"multiplication of "<<x<<" and "<<y<<" is "<<x*y<<endl;
+    cout<<"division of "<<x<<" and "<<y<<" is "<<x/y<<endl;
+    cout<<"modulus of "<<x<<" and "<<y<<" is "<<x%y<<endl;
+    return 0;
+}
