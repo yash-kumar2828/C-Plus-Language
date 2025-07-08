1f7522c1c0627ef557176b1cb90fa3ea187285cd
yash kumar <you@example.com>
2025-07-08 15:49:40 +0530

    type casting

---
+++ 08_typecasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter character=";
+    cin>>ch;
+    cout<<"ASCII value of this character  "<<ch<<" is "<<(int)ch;
+    return 0;
+}
