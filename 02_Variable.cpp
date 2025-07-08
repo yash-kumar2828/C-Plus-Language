a8c6af57da92f8a88ebac4308c07ca642a66286a
yash kumar <you@example.com>
2025-07-08 15:46:39 +0530

    variable

---
+++ 02_Variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    char str[10];
+    cout<<"enter a number=";
+    cin>>num;
+    cout<<"the number is "<<num<<endl;
+    cout<<"enter a word=";
+    cin>>str;
+    cout<<"the word is "<<str;
+    return 0;
+}
