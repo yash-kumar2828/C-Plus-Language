0d9b281dd71d49422544fbcf14c56998fa21baef
yash kumar <you@example.com>
2025-07-08 15:55:48 +0530

    ternary operator

---
+++ 20_ternary_operator.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter any positive number=";
+    cin>>num;
+    (num%2==0)?cout<<num<<" is a even number":cout<<num<<" is a odd number";
+}
