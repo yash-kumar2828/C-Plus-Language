c8405e55dcd0ef75459ad88ea51207ee9e986d12
yash kumar <you@example.com>
2025-07-08 15:53:02 +0530

    divisible by 5 and 3

---
+++ 14_divisible_by_five_three.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a number=";
+    cin>>num;
+    if(num%5==0 && num%3==0){
+        cout<<num<<" is a divisible by 5 and 3";
+    }else{
+        cout<<num<<" is not a divisible by 5 and 3";
+    }
+    return 0;
+}
