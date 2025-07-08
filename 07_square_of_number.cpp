ff28b702e07db091e30dbe6f9b62dccaa43aa458
yash kumar <you@example.com>
2025-07-08 15:49:16 +0530

    square of number

---
+++ 07_square_of_number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,sqr;
+    cout<<"enter a number=";
+    cin>>num;
+    sqr=num*num;
+    cout<<"the square of number "<<num<<" is "<<sqr;
+    return 0;
+}
