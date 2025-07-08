caf13fea6181ee09f00dc5cbc8a5c9247d49485b
yash kumar <you@example.com>
2025-07-08 15:54:26 +0530

    greatest number

---
+++ 17_greatest.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3;
+    cout<<"enter first number=";
+    cin>>n1;
+    cout<<"enter second number=";
+    cin>>n2;
+    cout<<"enter third number=";
+    cin>>n3;
+    if(n1>n2 && n1>n3){
+        cout<<n1<<" is a greatest number";
+    }else if(n2>n1 && n2>n3){
+        cout<<n2<<" is a greatest number";
+    }else{
+        cout<<n3<<" is a greatest number";
+    }
+    return 0;
+}
