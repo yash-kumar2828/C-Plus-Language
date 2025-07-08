cccc8e59cdc900fc2c607394c9d75d26ff601828
yash kumar <you@example.com>
2025-07-08 15:57:31 +0530

    display the AP

---
+++ 25_display_the_AP.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n=";
+    cin>>n;
+    for(int i=1;i<2*n-1;i+=2){
+        cout<<i<<" ";
+    }
+    return 0;
+}
