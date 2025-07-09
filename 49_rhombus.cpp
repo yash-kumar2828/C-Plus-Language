3feae3aa1f344903d650dbe999dfd8afbcc16776
yash kumar <you@example.com>
2025-07-09 11:55:46 +0530

    rhombus pattern

---
+++ 49_rhombus.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no of row=";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i+1;k++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
