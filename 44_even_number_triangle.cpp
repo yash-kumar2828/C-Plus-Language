98f9be2e1e066500962a03e79225f1654ae4bdce
yash kumar <you@example.com>
2025-07-09 11:25:00 +0530

    triangle pattern

---
+++ 44_even_number_triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,c;
+    cout<<"enter no of row=";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<2*j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
