29cc485f6c54c46b62a91f67029bbce142f71b54
yash kumar <you@example.com>
2025-07-09 12:06:50 +0530

    function

---
+++ 51_basic_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"good morning"<<endl;
+    cout<<"how are you?"<<endl;
+}
+int main(){
+    greet();
+    cout<<"hey"<<endl;
+    greet();
+    return 0;
+}
